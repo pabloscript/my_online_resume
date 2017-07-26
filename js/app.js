1d4c9823352b7d4a14e1f5dea6e827cf27f1c943
Pawel Kozicki <pe.kozicki@gmail.com>
2017-07-26 20:47:03 +0200

    dodane menu dla widoków mobilnych

--- js/app.js
+++ js/app.js
@@ -4,26 +4,55 @@ $(() => {
         event.preventDefault();
         $('body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top
-        }, 800);
+        }, 700);
     });
 
     // Akcje dla strzałek góra/dół
-    const containerDivs = $(".container");
+    const containerDivs = $(".container").next();
     console.log(containerDivs);
     const upArrow = $("#up");
     console.log(upArrow);
     const downArrow = $("#down");
     console.log(downArrow);
 
-    downArrow.on("click", () => {
-        console.log("Down");
-    });
-    upArrow.on("click", () => {
-        console.log("Up");
+    // downArrow.on("click", () => {
+    //     console.log("Down");
+    //     for (let i=0; i<containerDivs.length; i++) {
+    //         containerDivs.eq(i).hide(700);
+    //     }
+    // });
+
+    downArrow.click(function() {
+        var next;
+        next = $(this).parent().find(".container").next();
+        console.log(next);
+        $('html,body').animate({ scrollTop: next.offset().top
+        }, 1000);
     });
 
-    $('html, body').animate({
-        scrollTop: $(containerDivs).offset().top
-    }, 1000);
+
+
+    upArrow.on("click", () => {
+        console.log("Up");
+        $('html, body').animate({
+            scrollTop: $(containerDivs.prev()).offset().top
+        }, 1000);
+    });
+
+    // $(document).scroll(() => {
+    //     console.log("Scroll");
+    //     containerDivs.slideToggle();
+    //     $(this).next(containerDivs).slideToggle();
+    // //     $('html, body').animate({
+    // //         scrollTop: $(containerDivs.next()).offset().top
+    // //     }, 1000);
+    // });
+
+    $("#mobile-menu").on("click", () => {
+        $("#full-menu").slideToggle(500);
+        $("#full-menu").css("display", "flex");
+        // $($(this).find("a")).css("color", "white");
+    })
+
 
 })
--- js/out.js
+++ js/out.js
@@ -79,27 +79,53 @@ $(function () {
         event.preventDefault();
         $('body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top
-        }, 800);
+        }, 700);
     });
 
     // Akcje dla strzałek góra/dół
-    var containerDivs = $(".container");
+    var containerDivs = $(".container").next();
     console.log(containerDivs);
     var upArrow = $("#up");
     console.log(upArrow);
     var downArrow = $("#down");
     console.log(downArrow);
 
-    downArrow.on("click", function () {
-        console.log("Down");
-    });
-    upArrow.on("click", function () {
-        console.log("Up");
+    // downArrow.on("click", () => {
+    //     console.log("Down");
+    //     for (let i=0; i<containerDivs.length; i++) {
+    //         containerDivs.eq(i).hide(700);
+    //     }
+    // });
+
+    downArrow.click(function () {
+        var next;
+        next = $(this).parent().find(".container").next();
+        console.log(next);
+        $('html,body').animate({ scrollTop: next.offset().top
+        }, 1000);
     });
 
-    $('html, body').animate({
-        scrollTop: $(containerDivs).offset().top
-    }, 1000);
+    upArrow.on("click", function () {
+        console.log("Up");
+        $('html, body').animate({
+            scrollTop: $(containerDivs.prev()).offset().top
+        }, 1000);
+    });
+
+    // $(document).scroll(() => {
+    //     console.log("Scroll");
+    //     containerDivs.slideToggle();
+    //     $(this).next(containerDivs).slideToggle();
+    // //     $('html, body').animate({
+    // //         scrollTop: $(containerDivs.next()).offset().top
+    // //     }, 1000);
+    // });
+
+    $("#mobile-menu").on("click", function () {
+        $("#full-menu").slideToggle(500);
+        $("#full-menu").css("display", "flex");
+        // $($(this).find("a")).css("color", "white");
+    });
 });
 
 /***/ })
