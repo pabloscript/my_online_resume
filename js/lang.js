e83be12397891965e751e7dbce11cc213728f9c1
Pawel Kozicki <pe.kozicki@gmail.com>
2017-10-01 18:28:00 +0200

    Add English version of menu

--- js/lang.js
+++ js/lang.js
@@ -5,7 +5,7 @@ const pl = [
     "Następnie przez prawie 1,5 roku w tej samej firmie dbałem o jej wizerunek jako specjalista ds. komunikacji, głównie pisząc i redagując teksty po polsku i po angielsku.",
     "W połowie 2017 roku trafiłem na bootcamp programistyczny w Coders Lab, gdzie - w bardzo intensywny i skondensowany sposób - nauczyłem się: <br><br> HTML, CSS, JavaScript (w tym ES6), jQuery, React.",
     "Teraz jestem gotowy, żeby dalej rozwijać się jako Front-End Developer i uczyć nowych rzeczy. <br><br> Chcesz się ze mną skontaktować? Nic prostszego! <br><br> pe.kozicki (at) gmail.com"
-]
+];
 
 const en = [
     "Hi, I'm Paweł. <br><br> Click around the website to learn how I managed to become a front-end developer.",
@@ -14,7 +14,27 @@ const en = [
     "Then I switched positions within the same company where for over a year, as a communications specialist, I was focused on copywriting and proofreading in Polish and English.",
     "In the mid 2017 I attended a software dev bootcamp at Coders Lab where - in a very intensive and condensed manner - I learned: <br><br> HTML, CSS, JavaScript (including ES6), jQuery, React.",
     "Now I'm ready to further develop as a developer and learn new things. <br><br> If you wish to contact me - don't hesitate! <br><br> pe.kozicki (at) gmail.com"
-]
+];
+
+const plMenu = [
+    "Cześć",
+    "Edukacja",
+    "Rekruter",
+    "Copywriter",
+    "Programista",
+    "Kontakt"
+];
+
+const enMenu = [
+    "Hello",
+    "Education",
+    "Recruiter",
+    "Copywriter",
+    "Developer",
+    "Contact"
+];
 
 export {pl};
 export {en};
+export {plMenu};
+export {enMenu};
